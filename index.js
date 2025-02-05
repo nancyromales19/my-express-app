c04c195b7e23b99844fc56120e9b59ccbf960423
nancyromales19 <nancyromales16@gmail.com>
2025-02-06 01:06:15 +0800

    Error Handling Middleware

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.use((err, req, res, next) =>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.post('/submit', (req, res) =>{
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
